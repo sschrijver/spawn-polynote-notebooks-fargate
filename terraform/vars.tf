c513f81ece6fd3c584c471ebee59ccc21c9c20cf
Stephan Schrijver <stephanschrijver@gmail.com>
2020-05-17 13:01:32 +0200

    Update vars.tf

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -1,1 +1,12 @@
-
+variable "vpc_id" {}
+variable "region" {}
+variable "profile_name" {}
+variable "ecs_cluster_name" {}
+variable "loadbalancer_arn" {}
+variable "hosted_zone_id" {}
+variable "fargate_subnets" {
+  type = list(string)
+}
+variable "memory" {}
+variable "cpu" {}
+variable "domain" {}
